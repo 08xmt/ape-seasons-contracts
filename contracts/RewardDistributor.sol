c75a42b132307d6dfb9715b17aba722369f1aa08
matho <markus.08xmt@gmail.com>
2021-07-31 15:33:01 +0200

    Add Reward Disitributor contract

---
+++ contracts/RewardDistributor.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract RewardDistributor {
+
+    mapping(address => address) rewardToken;
+    mapping(address => uint) rewardAmount;
+
+    address owner;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    function addTournament(address _tournament, address _rewardToken, uint _rewardAmount) external{
+        require(owner == msg.sender);
+        rewardToken[_tournament] = _rewardToken;
+        rewardAmount[_tournament] = _rewardAmount;
+    }
+
+    function claim(address claimer) external returns(bool){
+        return IERC20(rewardToken[msg.sender]).transfer(claimer, rewardAmount[msg.sender]);
+    }
+
+    function getRewardToken() external view returns(address){
+        return rewardToken[msg.sender];
+    }
+
+    function getRewardAmount() external view returns (uint){
+        return rewardAmount[msg.sender];
+    }
+}
